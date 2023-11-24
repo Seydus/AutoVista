0ca2461516cb2e742db865ee6ad6f1335d2192dc
Seydus <weljohncatanpatan@gmail.com>
2023-11-25 02:37:07 +0800

    finish firebase integration

--- app/src/main/java/com/example/autovista/GlobalManager.java
+++ app/src/main/java/com/example/autovista/GlobalManager.java
@@ -4,16 +4,18 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.autovista.authentication.AuthenticationHandler;
 import com.example.autovista.authentication.FirebaseAuthentication;
 import com.example.autovista.remotedatabase.FirestoreHelper;
-import com.example.autovista.ui.UIManager;
+import com.example.autovista.ui.UIAuthentication;
 
 public class GlobalManager extends AppCompatActivity {
     public static GlobalManager Instance;
     private FirebaseAuthentication firebaseAuthentication;
     private FirestoreHelper firestoreHelper;
-    private UIManager uiAdapter;
-    
+    private UIAuthentication uiAdapter;
+    private AuthenticationHandler authenticationHandler;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +45,19 @@ public class GlobalManager extends AppCompatActivity {
         this.firestoreHelper = firestoreHelper;
     }
 
-    public UIManager getUiAdapter() {
+    public UIAuthentication getUiAdapter() {
         return uiAdapter;
     }
 
-    public void setUiAdapter(UIManager uiAdapter) {
+    public void setUiAdapter(UIAuthentication uiAdapter) {
         this.uiAdapter = uiAdapter;
+    }
+
+    public AuthenticationHandler getAuthenticationHandler() {
+        return authenticationHandler;
+    }
+
+    public void setAuthenticationHandler(AuthenticationHandler authenticationHandler) {
+        this.authenticationHandler = authenticationHandler;
     }
 }
--- app/src/main/java/com/example/autovista/models/ElectricClass.java
+++
@@ -1,7 +0,0 @@
-package com.example.autovista.models;
-
-public class ElectricClass {
-    int year;
-    String FName;
-    String LName;
-}
