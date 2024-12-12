d3bdd5a54a221b0f3bde1aba0f5167952b5eccd4
Jim5e <jamesterenceclarit@gmail.com>
2024-12-13 04:38:45 +0800

    added ui for the ff: - payment page - confirmation page
    To fix:
    - getting data from database
    
    To clean:
    redundant code such as userLogin check, database connections (idk abt this)

---
+++ app/src/main/java/com/example/autovista/SuccessfulTransaction.java
@@ -0,0 +1,15 @@
+package com.example.autovista;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessfulTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.succesful_transaction);
+        // Optionally, implement any additional logic or UI updates here
+    }
+}
